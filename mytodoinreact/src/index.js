cb8b33e2975b1839f0807ee157746251c6db1ecc
John Hudson <johnhudson@John-Hudsons-MacBook-Air.local>
2019-11-08 19:23:27 -0500

    first push - removing unnecessary files

---
+++ mytodoinreact/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+
+// This is grabbing the #root div on our index.html file and rendersing App component via app.js
+ReactDOM.render(<App />, document.getElementById('root'));
+
